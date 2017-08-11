772161d1d41017cb347825eb14d10e777dbf6897
Tipome <Tipome@users.noreply.github.com>
2017-08-11 15:06:35 +0200

    Update setup.py
    ajoute fichier lisez-moi et inclus les dll MSVisualBasic pour que le programme tourne sous tous les WIN

--- setup.py
+++ setup.py
@@ -3,12 +3,14 @@ from cx_Freeze import setup, Executable
 
 #Pour lancer la création de l'exe, taper dans la console "py setup.py build"
 #inclus les autres fichiers non python
-includefiles=["data.csv","Registre_type.xlsx","noms et trigrammes.xlsm","Forfaits HDC.xlsx"]
+includefiles=["data.csv","Registre_type.xlsx","noms et trigrammes.xlsm","Forfaits HDC.xlsx","lisezmoi.txt"]
 
 # Dependencies are automatically detected, but it might need fine tuning.
 build_exe_options = {"packages": ["os","openpyxl","pyexcel","pyexcel_xls","pyexcel_xlsx"],
                      "excludes": ["tkinter"],
-                     "include_files" : includefiles}
+                     "include_msvcr": True,  #skip error msvcr100.dll missing
+                     "include_files" : includefiles
+                     }
 
 # GUI applications require a different base on Windows (the default is for a
 # console application).
@@ -19,7 +21,7 @@ if sys.platform == "win32":
 
 
 setup(  name = "ControlHeure Vigie",
-        version = "0.3",
+        version = "0.8",
         description = "Crée les registres d'heures mensuels",
         options = {"build_exe": build_exe_options},
         executables = [Executable("ControlHeureVigie.py", icon="stopwatch.ico", base=base)])
